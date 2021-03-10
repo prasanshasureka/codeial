56484422ddd1b77818ec8cd43b4496a1b92b5174
prasanshasureka <sureka.prasansha@yahoo.com>
2021-03-10 12:52:46 +0530

    add multiple routes and controller

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 console.log('Router Running');
 const homeController = require('../controllers/home_controller');
+const userController = require('../controllers/user_controller');
+const { route } = require('./users');
 
 
 router.get('/', homeController.home);
+router.use('/user', require('./users'));
+router.use('/posts', require('./posts'));
+
 
 module.exports = router;
