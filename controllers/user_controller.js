8f39e5fe7024518f4e4eb41700bea359b0d9bb1b
prasanshasureka <sureka.prasansha@yahoo.com>
2021-03-11 12:49:31 +0530

    Add multiple controllers and views

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,8 @@
 module.exports.profile = function(req, res){
-    res.end('<h1>User Profile</h1>')
+    // res.end('<h1>User Profile</h1>');
+    return res.render('users', {
+        title: 'Users'
+    })
 }
 
 module.exports.posts = function(req, res){
