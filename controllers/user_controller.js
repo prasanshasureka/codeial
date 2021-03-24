909aa3c77012e25d9dde6369bab13466694cb762
prasanshasureka <sureka.prasansha@yahoo.com>
2021-03-24 22:30:03 +0530

    - add manual authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,9 +2,22 @@ const User = require('../models/user');
 
 module.exports.profile = function(req, res){
     // res.end('<h1>User Profile</h1>');
-    return res.render('users', {
-        title: 'Users'
-    })
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if (user){
+                return res.render('user_profile', {
+                    title: 'Profile',
+                    user: user
+                })
+            } else {
+                return res.redirect('/user/sign-in');
+            }
+        });
+    } else {
+        return res.redirect('/user/sign-in');
+    }
+    
+    
 }
 
 module.exports.posts = function(req, res){
@@ -50,5 +63,35 @@ module.exports.create = function(req, res){
 
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //TODO
+    // find the user 
+    User.findOne({
+        email: req.body.email
+    }, function (err, user){
+        if(err) {
+            console.log('error in finding user in signing in');
+            return
+        }
+        // handle user found
+        if (user){
+
+            // handle password incorrect
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            
+            res.cookie('user_id', user.id);
+            return res.redirect('/user/profile');
+
+        } else {
+            // handle user not found
+            
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.signOut = function(req, res){
+    res.clearCookie('user_id');
+    return res.redirect('/user/sign-in');
 }
