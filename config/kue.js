0f45720b0916107c37ecad96004ef4f4d3548b0a
prasanshasureka <sureka.prasansha@yahoo.com>
2021-05-18 23:34:45 +0530

    add kue ui to visualize delayed jobs

--- config/kue.js
+++ config/kue.js
@@ -1,4 +1,13 @@
 const kue = require('kue');
 const queue = kue.createQueue();
 
+
+// 
+var kueUiExpress = require('kue-ui-express');
+var express = require('express');
+var kueapp = express();
+kueUiExpress(kueapp, '/kue/', '/kue-api');
+kueapp.use('/kue-api/', kue.app);
+kueapp.listen(3000);
+
 module.exports = queue;
